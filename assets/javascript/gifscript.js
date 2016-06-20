a06564fdd0be7c8bf1c79ea30db4db77120636af
Ryan Jarrell <Ryan Jarrell>
2016-06-20 18:53:03 -0400

    everything working before I try to add local storage

--- assets/javascript/gifscript.js
+++ assets/javascript/gifscript.js
@@ -12,7 +12,7 @@ function addButton(celebName) {
 		type: 'button',
 		text: celebName,
 		class: 'btn btn-primary celebButton',
-		'data-celeb': celebName
+		data: {celeb: celebName}
 	}).appendTo($('#celebButtonsEl'));
 };
 
@@ -23,16 +23,24 @@ for (i = 0; i < celebrities.length; i++) {
 //ADD A NEW BUTTON
 $('#celebForm').on('submit', function () {
 	var celebToAdd = $('#celebInput').val();
-	addButton(celebToAdd);
+
+	//error checking - blank or already has a button
+	if (!celebToAdd) {
+		alert("Please type something!");
+		return;
+	} else if (celebrities.indexOf(celebToAdd) !== -1) {
+		alert("There's already a button for that!");
+		return;
+	} else {
+		var celebToAdd = $('#celebInput').val();
+		addButton(celebToAdd);
+		celebrities.push(celebToAdd);
+	}
 });
 
 //AJAX CALL
-$('#celebButtonsEl').on('click', '.celebButton', function () {
-
-	// $('.gifContainerEl').delete();
-
+$('#celebButtonsEl').on('click', '.celebButton', function () {	
 	var celebToSearch = $(this).data('celeb');
-
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+celebToSearch+"&api_key="+apiKey+"&limit=10&rating=pg";
 
 	$.ajax({url: queryURL, method: 'GET'}).done(function (response) {
@@ -40,15 +48,11 @@ $('#celebButtonsEl').on('click', '.celebButton', function () {
 
 		$('.gifsBigDiv').remove();
 			
-		$('<div>').attr('class', 'gifsBigDiv').appendTo('.container-fluid');
+		var gifsBigDiv = $('<div>').attr('class', 'gifsBigDiv');
 
 		for (i = 0; i < response.data.length; i++) {
-
-	
-
 			//display rating
 			var rating = response.data[i].rating.toUpperCase(); 
-
 			var ratingEl = $('<div>').attr('class', 'rating').text('Rating: '+rating);
 
 			//display gif
@@ -57,23 +61,24 @@ $('#celebButtonsEl').on('click', '.celebButton', function () {
 			console.log(gif);
 
 			var still = response.data[i].images.fixed_height_still.url;
-			var gifEl = $('<img>').attr({src: gif,
-										 alt: alt,
-										 class: 'gif', 
-										 'data-still': still,
-										 'data-animation': gif,
-										 'data-paused': false
+			var gifEl = $('<img>', {
+							src: gif,
+							alt: alt,
+							class: 'gif', 
+							data: {still: still, animation: gif, paused: false},
+							// data: {animation: gif},
+							// data: {paused: false}
 						});
 
 			var gifContainerEl = $('<div>').attr('class', 'gifContainer');
 
-			gifContainerEl.appendTo($('.gifsBigDiv'));
 			ratingEl.appendTo(gifContainerEl);
 			gifEl.appendTo(gifContainerEl);
-			
-		
-
+			gifContainerEl.appendTo(gifsBigDiv);
 		}
+
+		//only paints to the DOM per say one time w00t
+		$('#gifsForScreen').html(gifsBigDiv);
 	});
 });
 
