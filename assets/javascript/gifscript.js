e4a4dc8e1b6f00aca51a139b4764fb3076c2481d
Ryan Jarrell <Ryan Jarrell>
2016-06-20 21:56:55 -0400

    I think it's done now

--- assets/javascript/gifscript.js
+++ assets/javascript/gifscript.js
@@ -1,41 +1,94 @@
-var apiKey = 'dc6zaTOxFJmzC'
+var apiKey = 'dc6zaTOxFJmzC';
 
 var celebrities = ['Bill Murray', 'Jennifer Lawrence', 'Ricky Gervais', 'Adam Levine', 'Robert Downey Jr', 'Brad Pitt',
 			'Bradley Cooper', 'Aziz Ansari', 'Conan OBrien', 'John Mayer', 'Chuck Norris', 'Charlie Scheen',
 			'Will Ferrell', 'Leonardo DiCaprio', 'Amy Poehler', 'Paul Rudd', 'Johnny Depp', 'Kristen Wiig'];
 
-//check to see if search query is already in array
-//don't let them submit blank
-
-function addButton(celebName) {
-	$('<button>', {
-		type: 'button',
-		text: celebName,
-		class: 'btn btn-primary celebButton',
-		data: {celeb: celebName}
-	}).appendTo($('#celebButtonsEl'));
+function addCeleb(celebName) {
+	var celebritiesNew = JSON.parse(localStorage.getItem("celebsLocal"));
+	celebritiesNew.push(celebName);
+	localStorage.setItem("celebsLocal", JSON.stringify(celebritiesNew));	
 };
 
-for (i = 0; i < celebrities.length; i++) {
-	addButton(celebrities[i]);
+function checkForLocalArray(arrayName) {
+	var celebs = JSON.parse(localStorage.getItem(arrayName));
+	if (!celebs) {
+		return false;
+	} else {
+		return true;
+	}
 };
+
+//adds all buttons at once
+function addButtons() {
+	//checks if there is already an array of celebs saved locally
+	if(!checkForLocalArray('celebsLocal')) {
+		localStorage.setItem('celebsLocal', JSON.stringify(celebrities));
+	}
+	
+	var buttonContainer = $('<div>', {class: 'buttonContainer'});
+	var celebs = JSON.parse(localStorage.getItem("celebsLocal"));
+	for (var i = 0; i < celebs.length; i++) {
+		var fullButton = $('<div>',
+			{class: 'fullButton',
+			'data-celebindex': i, 
+			'data-celebname': celebs[i]
+		});
+
+		var celebButton = $('<button>', {	
+			type: 'button',
+			text: celebs[i],
+			class: 'btn btn-primary celebButton',
+			data: {celeb: celebs[i]}
+		});
+
+		var celebX = $('<button>', {
+			type: 'button',
+			text: 'X',
+			class: 'checkbox',
+			'data-celebindex': i, 
+			'data-celebname': celebs[i]			
+		});
+
+		celebButton.appendTo(fullButton);
+		celebX.appendTo(fullButton);
+		fullButton.appendTo(buttonContainer);
+	}	
+	($('#celebButtonsEl')).html(buttonContainer);
+};	
+
+function deleteButton (celebIndex) {
+	var celebs = JSON.parse(localStorage.getItem('celebsLocal'));
+	celebs.splice(celebIndex, 1);
+	localStorage.setItem('celebsLocal',JSON.stringify(celebs));
+	addButtons();
+}; 
+
+//initial display of buttons on screen
+addButtons();
 
 //ADD A NEW BUTTON
 $('#celebForm').on('submit', function () {
 	var celebToAdd = $('#celebInput').val();
+	var celebs = JSON.parse(localStorage.getItem('celebsLocal'));
 
 	//error checking - blank or already has a button
 	if (!celebToAdd) {
 		alert("Please type something!");
 		return;
-	} else if (celebrities.indexOf(celebToAdd) !== -1) {
+	} else if (celebs.indexOf(celebToAdd) !== -1) {
 		alert("There's already a button for that!");
 		return;
 	} else {
-		var celebToAdd = $('#celebInput').val();
-		addButton(celebToAdd);
-		celebrities.push(celebToAdd);
+		addCeleb(celebToAdd);
+		addButtons();
 	}
+	$('#celebInput').val('');
+});
+
+//DELETE A BUTTON
+$('#celebButtonsEl').on('click', '.checkbox', function() {
+	deleteButton($(this).data('celebindex'));
 });
 
 //AJAX CALL
@@ -44,12 +97,10 @@ $('#celebButtonsEl').on('click', '.celebButton', function () {
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+celebToSearch+"&api_key="+apiKey+"&limit=10&rating=pg";
 
 	$.ajax({url: queryURL, method: 'GET'}).done(function (response) {
-		console.log(response);
-
 		$('.gifsBigDiv').remove();
-			
 		var gifsBigDiv = $('<div>').attr('class', 'gifsBigDiv');
 
+		//loops through response and displays gifs
 		for (i = 0; i < response.data.length; i++) {
 			//display rating
 			var rating = response.data[i].rating.toUpperCase(); 
@@ -58,17 +109,16 @@ $('#celebButtonsEl').on('click', '.celebButton', function () {
 			//display gif
 			var alt = celebToSearch+" Image # "+(i + 1);
 			var gif = response.data[i].images.fixed_height.url;
-			console.log(gif);
 
 			var still = response.data[i].images.fixed_height_still.url;
 			var gifEl = $('<img>', {
-				src: gif,
-				alt: alt,
-				class: 'gif', 
-				data: {still: still, animation: gif, paused: false},
-				// data: {animation: gif},
-				// data: {paused: false}
-			});
+							src: gif,
+							alt: alt,
+							class: 'gif', 
+							data: {still: still, animation: gif, paused: false},
+							// data: {animation: gif},
+							// data: {paused: false}
+						});
 
 			var gifContainerEl = $('<div>').attr('class', 'gifContainer');
 
@@ -76,8 +126,7 @@ $('#celebButtonsEl').on('click', '.celebButton', function () {
 			gifEl.appendTo(gifContainerEl);
 			gifContainerEl.appendTo(gifsBigDiv);
 		}
-
-		//only paints to the DOM per say one time w00t
+		//only paints to the DOM par say one time w00t
 		$('#gifsForScreen').html(gifsBigDiv);
 	});
 });
@@ -93,6 +142,6 @@ $('.container-fluid').on('click', '.gif', function() {
 });
 
 
-// var gif = response.data[]
+
 
 
